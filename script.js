d1d7fc7ae62c722fb4e4775f5e494fdf481dd976
Ward Streicher <8474691+wstreicher@users.noreply.github.com>
2022-11-16 05:46:02 -0500

    Add fill styles

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let newGridButton = document.querySelector('.new-grid');
 newGridButton.addEventListener('click', generateNewGrid);
 
-generateGrid(16);
-
-
+generateGrid(16, "black");
 
 // Removes the current grid, if possible, and creates a new grid with the specified number of rows and column.
-function generateGrid(num) {
+function generateGrid(num, style) {
+    console.log(style)
     let container = document.querySelector('.container');
     while (container.firstChild) {
         container.replaceChildren(); 
@@ -17,27 +16,46 @@ function generateGrid(num) {
        let newCell = document.createElement('div');
        newCell.classList.add('cell');
        container.appendChild(newCell);
-       newCell.addEventListener('mouseover', changeColor);
+       if (style == "random") {
+        newCell.addEventListener('mouseover', changeColorRandom);
+       } else if (style == "darken") {
+        newCell.addEventListener('mouseover', changeColorDarken);
+       } else {
+        newCell.addEventListener('mouseover', changeColorBlack)
+       }
     };
 
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-
-
 };
 
 // Prompts the user for input, which it uses to generate a new grid.
 function generateNewGrid() {
-    let input = parseInt(window.prompt("Enter a number between 1-100:"));
+    let gridInput = parseInt(window.prompt("Enter a number between 1-100:"));
+    let styleInput = window.prompt('Please specify the style: ("black", "random", or "darken." Will default to "black.")').toLowerCase();
+
     // Defaults to 16 for invalid inputs.
-    if ((input < 1) || (input > 100) || (isNaN(input))){
-        input = 16;
+    if ((gridInput < 1) || (gridInput > 100) || (isNaN(gridInput))){
+        gridInput = 16;
     };
-    generateGrid(input);
+
+    generateGrid(gridInput, styleInput);
 };
 
-// Changes the color of a cell when it is moused over.
-function changeColor() {
+// Changes the color of a cell to black when it is moused over.
+function changeColorBlack() {
     this.style.backgroundColor = "black";
-}
+};
 
+// Changes the color of a cell to a randomly-generated color when it is moused over.
+function changeColorRandom() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    this.style.backgroundColor = `#${randomColor}`;
+};
+
+// Changes the color of a cell by slightly darkening it 10% at a time.
+function changeColorDarken() {
+    let change = parseFloat(window.getComputedStyle(this).backgroundColor.match(/[^,]+(?=\))/));
+    let change2 = window.getComputedStyle(this).backgroundColor.replace(/[^,]+(?=\))/, `${change + .1}`);
+    this.style.backgroundColor = change2;
+}
